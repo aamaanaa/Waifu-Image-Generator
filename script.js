84bc571bdb0c49efe6d005466ddb6c30bafae312
Aamaanaa <55531822+aamaanaa@users.noreply.github.com>
2026-04-22 00:44:07 +0200

    Fix incorrect api, incorrect array

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const waifuImages = document.getElementById('waifuImages');
 getWaifuImage();
 
 function getWaifuImage() {
-    fetch('https://api.waifu.im/search/?excluded_files=4401')
+    fetch('https://api.waifu.im/images')
     .then((response) => response.json())
     .then((data) => {
         waifuImages.innerHTML = `
-            <img src="${data.images[0].url}">
-            <p>Source: <a href="${data.images[0].source}">${data.images[0].source}</a></p>
+            <img src="${data.items[0].url}">
+            <p>Source: <a href="${data.items[0].source}">${data.items[0].source}</a></p>
             <button class="glow-on-hover" onclick="getWaifuImage()">Get Waifu Image</button>
         `;
     })
